5193574c90b196b47d78af0eb3794c2cc4dd5374
Lê Xuân Gia Hân <lxgh@netcompany.com>
2023-05-11 15:39:56 +0700

    Attempting pushing all without the large file

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    # The name of your Terraform Cloud organization.
+    organization = "hanxgle"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "learn-lxgh"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "learn-lxgh-rg"
+  location = "southeastasia"
+}
